ffe0a26aae19e94a17ab4746855eec7f53264894
Miika Henttonen <miika.henttonen@wunderdog.fi>
2020-04-07 14:11:12 +0300

    Test that lint-staged and eslint work

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,11 @@
 // @ts-ignore
 import { serve } from "https://deno.land/std/http/server.ts";
-const s = serve({ port: 8000 });
-console.log("http://localhost:8000/");
+
+const PORT = 8000;
+
+const s = serve({ port: PORT });
+console.log(`Serving http://localhost:${PORT}/`);
+
 // @ts-ignore
 for await (const req of s) {
   req.respond({ body: "Hello World\n" });
